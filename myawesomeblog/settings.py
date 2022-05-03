2f4bb4e12929e49d06027969c01fed40ef43797e
mishapravdiuk <mishapravdiuk.com>
2022-05-03 12:11:07 +0300

    Final version of test myawesomeblog

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -121,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+
+    BASE_DIR / 'myawesomeblog/static/'
+
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
